d681b7a69aafca1d84c116d23c15dd513e5cfed9
Timo van den Boom <timo_boom1@hotmail.com>
2016-05-18 22:27:36 +0200

    Add main method docs

--- src/main/java/me/childintime/ChildInTime.java
+++ src/main/java/me/childintime/ChildInTime.java
@@ -2,6 +2,11 @@ package me.childintime;
 
 public class ChildInTime {
 
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start up arguments.
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
