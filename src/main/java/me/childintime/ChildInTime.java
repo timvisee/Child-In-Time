0bb75198ee483e8144d907ce508309bd5241fe57
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-05-18 22:34:48 +0200

    Update console message

--- src/main/java/me/childintime/ChildInTime.java
+++ src/main/java/me/childintime/ChildInTime.java
@@ -3,6 +3,6 @@ package me.childintime;
 public class ChildInTime {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to Child in Time v0.1");
     }
 }
