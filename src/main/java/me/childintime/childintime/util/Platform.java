ad87f39177a91110cdf0ab18ec4843f9254dc94e
Tim Visée <timvisee@gmail.com>
2016-05-19 21:56:58 +0200

    Improve Platform class

--- src/main/java/me/childintime/childintime/util/Platform.java
+++ src/main/java/me/childintime/childintime/util/Platform.java
@@ -32,20 +32,62 @@ public enum Platform {
 	 * @return Platform.
      */
 	public static Platform getPlatform() {
-		String osName = System.getProperty("os.name").toLowerCase();
+		// Get the operating system name
+		String os = System.getProperty("os.name").toLowerCase();
 
-		if (osName.contains("win"))
-			return me.childintime.childintime.util.Platform.WINDOWS;
+		// Check whether we're running on Windows
+		if(os.contains("win"))
+			return WINDOWS;
 
-		if (osName.contains("mac"))
-			return me.childintime.childintime.util.Platform.MAC_OS_X;
+		// Check whether we're running on Linux
+		if (os.contains("linux") || os.contains("unix"))
+			return LINUX;
 
-		if (osName.contains("solaris") || osName.contains("sunos"))
-			return me.childintime.childintime.util.Platform.SOLARIS;
+		// Check whether we're running on Mac OS X
+		if (os.contains("mac"))
+			return MAC_OS_X;
 
-		if (osName.contains("linux") || osName.contains("unix"))
-			return me.childintime.childintime.util.Platform.LINUX;
+		// Check whether we're running on Solaris
+		if (os.contains("solaris") || os.contains("sunos"))
+			return SOLARIS;
 
-		return me.childintime.childintime.util.Platform.UNKNOWN;
+		// We're running on an unknown operating system
+		return UNKNOWN;
+	}
+
+	/**
+	 * Check whether the application is running on a Windows machine.
+	 *
+	 * @return True if the application is running on a Windows machine, false if not.
+	 */
+	public static boolean isWindows() {
+		return getPlatform() == WINDOWS;
+	}
+
+	/**
+	 * Check whether the application is running on a Linux machine.
+	 *
+	 * @return True if the application is running on a Linux machine, false if not.
+	 */
+	public static boolean isLinux() {
+		return getPlatform() == LINUX;
+	}
+
+	/**
+	 * Check whether the application is running on a Mac OS X machine.
+	 *
+	 * @return True if the application is running on a Mac OS X machine, false if not.
+	 */
+	public static boolean isMacOsX() {
+		return getPlatform() == MAC_OS_X;
+	}
+
+	/**
+	 * Check whether the application is running on a Solaris machine.
+	 *
+	 * @return True if the application is running on a Solaris machine, false if not.
+	 */
+	public static boolean isSolaris() {
+		return getPlatform() == SOLARIS;
 	}
 }
