d0705a8783d894a9a14990f8ffcf0bd188e0195a
Tim Visée <timvisee@gmail.com>
2016-05-20 16:45:19 +0200

    Remove copyright from forms

--- src/main/java/me/childintime/childintime/database/DatabaseEditForm.java
+++ src/main/java/me/childintime/childintime/database/DatabaseEditForm.java
@@ -1,15 +1,4 @@
-/**
- * TestTheTeacher.
- * Test the teacher to see whether he or she is really good enough at basing programming.
- * A project for a school of applied sciences.
- *
- * @author MingYan Li, Tim Visee
- * @copyright Copyright (c) MingYan Li & Tim Visee 2015. All rights reserved.
- * @license GNU GPL v3.0
- */
-
 package me.childintime.childintime.database;
-
 
 import me.childintime.childintime.App;
 
