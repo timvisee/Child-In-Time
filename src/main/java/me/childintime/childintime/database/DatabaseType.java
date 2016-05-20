40db3550af8cdff83a0d1b7519ccf6a99feea0b1
Tim Visée <timvisee@gmail.com>
2016-05-21 00:25:54 +0200

    Add dynamic class types to DatabaseType enum

--- src/main/java/me/childintime/childintime/database/DatabaseType.java
+++ src/main/java/me/childintime/childintime/database/DatabaseType.java
@@ -4,12 +4,12 @@ public enum DatabaseType {
     /**
      * Integrated (local) database.
      */
-    INTEGRATED(1, "Integrated database"),
+    INTEGRATED(1, "Integrated database", IntegratedDatabase.class),
 
     /**
      * Remote database.
      */
-    REMOTE(2, "Remote database");
+    REMOTE(2, "Remote database", RemoteDatabase.class);
 
     /**
      * Database type ID.
@@ -22,14 +22,20 @@ public enum DatabaseType {
     private String description;
 
     /**
+     * Type class.
+     */
+    private Class<? extends AbstractDatabase> typeClass;
+
+    /**
      * Constructor.
      *
      * @param typeId Database type ID.
      * @param description Description.
      */
-    DatabaseType(int typeId, String description) {
+    DatabaseType(int typeId, String description, Class<? extends AbstractDatabase> typeClass) {
         this.typeId = typeId;
         this.description = description;
+        this.typeClass = typeClass;
     }
 
     /**
@@ -56,6 +62,30 @@ public enum DatabaseType {
      */
     public int getTypeId() {
         return this.typeId;
+    }
+
+    /**
+     * Get the type class.
+     *
+     * @return Type class.
+     */
+    public Class<? extends AbstractDatabase> getTypeClass() {
+        return this.typeClass;
+    }
+
+    /**
+     * Determine the index of the current database type.
+     *
+     * @return Database type index.
+     */
+    public int getIndex() {
+        // Loop through the list of values, and compare it to this one, to determine the index
+        for(int i = 0; i < values().length; i++)
+            if(values()[i].equals(this))
+                return i;
+
+        // Unable to find the index, return zero
+        return 0;
     }
 
     @Override
