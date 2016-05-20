f2bd4e80c190aa4e80e52518b9887f5a36f9bc2d
Tim Visee <timvisee@gmail.com>
2016-05-20 12:57:48 +0200

    Create integrated database class

---
+++ src/main/java/me/childintime/childintime/database/IntegratedDatabase.java
@@ -0,0 +1,68 @@
+package me.childintime.childintime.database;
+
+import com.timvisee.yamlwrapper.configuration.ConfigurationSection;
+
+import java.io.File;
+
+public class IntegratedDatabase extends AbstractDatabase {
+
+    /**
+     * Database file to use.
+     */
+    private File file;
+
+    /**
+     * Configuration section to load the database from.
+     *
+     * @param config Configuration section.
+     */
+    public IntegratedDatabase(ConfigurationSection config) {
+        // Call the super
+        super(config);
+
+        // Fetch the file from the configuration
+        String rawFile = config.getString("file");
+
+        // Determine the file
+        if(rawFile != null)
+            this.file = new File(rawFile);
+    }
+
+    /**
+     * Get the database file.
+     *
+     * @return Database file, or null if not configured.
+     */
+    public File getFile() {
+        return this.file;
+    }
+
+    /**
+     * Check whether a file has been configured.
+     *
+     * @return True if a file has been configured.
+     */
+    public boolean hasFile() {
+        return this.file != null;
+    }
+
+    /**
+     * Set the database file.
+     *
+     * @param file Database file.
+     */
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void save(ConfigurationSection config) {
+        // Save the file path
+        config.set("file", file.getAbsolutePath());
+    }
+
+    @Override
+    public DatabaseType getType() {
+        return DatabaseType.INTEGRATED;
+    }
+}
