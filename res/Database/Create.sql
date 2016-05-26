7082c7989c56b774ff90df33eda13a39220fc870
Tim Visee <timvisee@gmail.com>
2016-05-26 15:26:44 +0200

    Fix meta data creation procedure, make SQL types consistent

--- res/Database/Create.sql
+++ res/Database/Create.sql
@@ -1,127 +1,134 @@
-DROP DATABASE `childintime`;
+DROP DATABASE IF EXISTS `childintime`;
 CREATE DATABASE `childintime`;
 USE childintime;
 
 create table `school` (
-`id` int(9) NOT NULL AUTO_INCREMENT,
-    	`name` varchar(30) NOT NULL,
-    	`commune` varchar(30) NOT NULL,
+`id` INT NOT NULL AUTO_INCREMENT,
+    	`name` TEXT NOT NULL,
+    	`commune` TEXT NOT NULL,
     	PRIMARY KEY (`id`)
  );
 
-
 create table `teacher` (
-	`id` int(9) NOT NULL AUTO_INCREMENT,
-    	`first_name` varchar(30) NOT NULL,
-    	`last_name` varchar(30) NOT NULL,
-    	`is_gym` char(1) NOT NULL,
-    	`school_id` int(9) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
+    	`first_name` TEXT NOT NULL,
+    	`last_name` TEXT NOT NULL,
+    	`is_gym` TINYINT NOT NULL,
+    	`school_id` INT NOT NULL,
     	PRIMARY KEY (`id`),
     	FOREIGN KEY (`school_id`) REFERENCES `school`(`id`)
-
 );
 
-create table `group` (
-	`id` int(9) NOT NULL AUTO_INCREMENT,
-	`name` varchar(30) NOT NULL,
-    	`school_id` int(9) NOT NULL,
+CREATE TABLE `group` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` TEXT NOT NULL,
+    	`school_id` INT NOT NULL,
     	PRIMARY KEY (`id`),
     	FOREIGN KEY (`school_id`) REFERENCES `school`(`id`)
 );
 
 create table `student` (
-	`id` int(9) NOT NULL AUTO_INCREMENT,
-    	`first_name` varchar(30) NOT NULL,
-    	`last_name` varchar(30) NOT NULL,
-    	`gender` int(1) NOT NULL,
-    	`birthdate` date NOT NULL,
-    	`group_id` int(9) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
+    	`first_name` TEXT NOT NULL,
+    	`last_name` TEXT NOT NULL,
+    	`gender` TINYINT NOT NULL,
+    	`birthdate` DATE NOT NULL,
+    	`group_id` INT NOT NULL,
     	PRIMARY KEY (`id`),
     	FOREIGN KEY (`group_id`) REFERENCES `group`(`id`)
 
 );
 
 create table `bodystate` (
-	`student_id` int(9) NOT NULL,
-    	`date` date NOT NULL,
-    	`length` decimal(2,1) NOT NULL,
-    	`weight` int(3) NOT NULL,
+	`student_id` INT NOT NULL,
+    	`date` DATE NOT NULL,
+    	`length` SMALLINT NOT NULL,
+    	`weight` SMALLINT NOT NULL,
     	PRIMARY KEY (`student_id`, `date`),
     	FOREIGN KEY (`student_id`) REFERENCES `student`(`id`)
 );
 
 create table `parkour` (
-	`id` int(9) NOT NULL AUTO_INCREMENT,
-    	`description` varchar(200) NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
+    	`description` TEXT NULL,
     	PRIMARY KEY (`id`)
 );
 
 create table `measurement` (
-	`student_id` int(9) NOT NULL,
-	`date` date NOT NULL,
-    	`time` time NOT NULL,
-    	`parkour_id` int(9) NOT NULL,
-   	PRIMARY KEY (`student_id`, `date`),
-    	FOREIGN KEY	(`student_id`) REFERENCES `student`(`id`),
-    	FOREIGN KEY (`parkour_id`) REFERENCES `parkour`(`id`)
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`student_id` INT NOT NULL,
+	`date` DATE NOT NULL,
+    	`time` TIME NOT NULL,
+    	`parkour_id` INT NOT NULL,
+   	PRIMARY KEY (`id`),
+		FOREIGN KEY	(`student_id`) REFERENCES `student`(`id`),
+		FOREIGN KEY (`parkour_id`) REFERENCES `parkour`(`id`)
 );
 
 create table `group_teacher` (
-	`group_id` int(9) NOT NULL,
-    	`teacher_id` int(9) NOT NULL,
+	`group_id` INT NOT NULL,
+    	`teacher_id` INT NOT NULL,
     	PRIMARY KEY (`group_id`, `teacher_id`),
     	FOREIGN KEY (`group_id`) REFERENCES `group`(`id`),
     	FOREIGN KEY (`teacher_id`) REFERENCES `teacher`(`id`)
 );
 
+# Drop the create meta table statement if it already exists
+DROP PROCEDURE IF EXISTS createMetaTable;
 
-
-
-create procedure createMetaTable(tableName VARCHAR(30))
+# Create the create meta table statement, to easily create metadata tables
+CREATE PROCEDURE createMetaTable(IN tableName VARCHAR(30))
 	BEGIN
 
-		PREPARE table1 FROM 'CREATE TABLE `?` (
-			`id` int(9) NOT NULL,
-			`field` varchar(30) NOT NULL,
-			`type` varchar(30) NOT NULL,
-			`value` varchar(30) NULL,
-			`?` int(9) NOT NULL,
+		# Build the _meta_data statement
+		SET @metaDataStatement = CONCAT('CREATE TABLE `', tableName, '_meta_data` (
+			`id` INT NOT NULL,
+			`field` TEXT NOT NULL,
+			`type` SMALLINT NOT NULL,
+			`value` TEXT NULL,
+			`', tableName, '_id` int(9) NOT NULL,
 			PRIMARY KEY (`id`),
-			FOREIGN KEY (`?`) REFERENCES `?`(`id`)
-		);';
+			FOREIGN KEY (`', tableName, '_id`) REFERENCES `', tableName, '`(`id`)
+		);');
 
-		PREPARE table2 FROM 'CREATE TABLE `?` (
-			`id` int(9) NOT NULL,
-			`name` varchar(30) NOT NULL,
-			`type` varchar(30) NOT NULL,
-			`default` varchar(30) NOT NULL,
-			`allowNull` enum(''Y'', ''N''),
+		# Build the _meta_field statement
+		SET @metaFieldStatement = CONCAT('CREATE TABLE `', tableName, '_meta_field` (
+			`id` INT NOT NULL,
+			`name` TEXT NOT NULL,
+			`type` SMALLINT NOT NULL,
+			`default` TEXT NULL,
+			`allow_null` TINYINT DEFAULT 1 NOT NULL,
+			PRIMARY KEY (`id`)
+		);');
+
+		# Build the _meta_value statement
+		SET @metaValueStatement = CONCAT('CREATE TABLE `', tableName, '_meta_value` (
+			`id` INT NOT NULL,
+			`value` TEXT NOT NULL,
+			`field_id` INT NOT NULL,
 			PRIMARY KEY (`id`),
-		);';
+			FOREIGN KEY (`field_id`) REFERENCES `', tableName,'_meta_field`(`id`)
+		);');
 
-		PREPARE table3 FROM 'CREATE TABLE `?` (
-			`id` int(9) NOT NULL,
-			`value` varchar(30) NOT NULL,
-			`field_id` int(9) NOT NULL,
-			PRIMARY KEY (`id`),
-			FOREIGN KEY (`field_id`) REFERENCES `?`(`id`)
-		);';
+		# Prepare the statements
+		PREPARE metaDataPrepared FROM @metaDataStatement;
+		PREPARE metaFieldPrepared FROM @metaFieldStatement;
+		PREPARE metaValuePrepared FROM @metaValueStatement;
 
+		# Execute the statements
+		EXECUTE metaDataPrepared;
+		EXECUTE metaFieldPrepared;
+		EXECUTE metaValuePrepared;
 
-		SET @a = CONCAT(tableName, '_meta_data');
-		SET @b = CONCAT(tableName, '_id');
-		EXECUTE table1 USING @a, @b, @b, tableName;
-
-		SET @c = CONCAT(tableName, '_meta_field');
-		EXECUTE table2 USING @c;
-
-		SET @d = CONCAT(tableName, '_meta_value');
-		SET @e = CONCAT(tableName, '_meta_field');
-		EXECUTE table3 using @d, @e;
+		# Deallocate the statements
+		DEALLOCATE PREPARE metaDataPrepared;
+		DEALLOCATE PREPARE metaFieldPrepared;
+		DEALLOCATE PREPARE metaValuePrepared;
 
 	END;
 ;
 
+# Create meta data tables for database objects
 CALL createMetaTable('student');
 CALL createMetaTable('teacher');
 CALL createMetaTable('group');
@@ -129,8 +136,5 @@ CALL createMetaTable('school');
 CALL createMetaTable('parkour');
 CALL createMetaTable('measurement');
 
-
-
-
-
-
+# Drop the create meta table procedure, we aren't using it anymore
+DROP PROCEDURE createMetaTable;
