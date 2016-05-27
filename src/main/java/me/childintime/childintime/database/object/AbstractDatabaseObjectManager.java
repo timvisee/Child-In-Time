ec85d0db1f9a1f93ccbaf616f145d33622b32c0d
Tim Visee <timvisee@gmail.com>
2016-05-27 12:40:49 +0200

    Create temporary abstract database object manager class

---
+++ src/main/java/me/childintime/childintime/database/object/AbstractDatabaseObjectManager.java
@@ -0,0 +1,7 @@
+package me.childintime.childintime.database.object;
+
+public abstract class AbstractDatabaseObjectManager {
+
+    // TODO: Create the body of this object!
+    // TODO: Shorten the name of this object?
+}
