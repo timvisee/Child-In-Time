792584405e7aeadd277b97596e0ec0002cf3df4f
Tim Visee <timvisee@gmail.com>
2016-05-30 13:13:39 +0200

    Add JavaDocs to TeacherFields enum

--- src/main/java/me/childintime/childintime/database/object/teacher/TeacherFields.java
+++ src/main/java/me/childintime/childintime/database/object/teacher/TeacherFields.java
@@ -3,11 +3,26 @@ package me.childintime.childintime.database.object.teacher;
 import me.childintime.childintime.database.object.DatabaseFieldsInterface;
 
 public enum TeacherFields implements DatabaseFieldsInterface {
+
+    // TODO: Put actual teacher fields here!
     ;
+
+    /**
+     * Field name in the database of the field.
+     */
+    private String fieldName;
+
+    /**
+     * Constructor.
+     *
+     * @param fieldName Database field name.
+     */
+    TeacherFields(String fieldName) {
+        this.fieldName = fieldName;
+    }
 
     @Override
     public String getFieldName() {
-        return null;
+        return this.fieldName;
     }
-    // TODO: implement this
 }
