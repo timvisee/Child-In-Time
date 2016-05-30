95496ec508c57fe7c53f6f2988e9713a7e784828
Tim Visee <timvisee@gmail.com>
2016-05-30 13:11:09 +0200

    Add JavaDocs to StudentFields

--- src/main/java/me/childintime/childintime/database/object/student/StudentFields.java
+++ src/main/java/me/childintime/childintime/database/object/student/StudentFields.java
@@ -4,16 +4,36 @@ import me.childintime.childintime.database.object.DatabaseFieldsInterface;
 
 public enum StudentFields implements DatabaseFieldsInterface {
 
+    /**
+     * Student ID.
+     */
     ID ("id"),
+
+    /**
+     * First name of the student.
+     */
     FIRST_NAME ("name");
 
-    private String fieldname;
+    /**
+     * Field name in the database of the field.
+     */
+    private String fieldName;
 
-    StudentFields(String fieldname) {
-        this.fieldname = fieldname;
+    /**
+     * Constructor.
+     *
+     * @param fieldName Field name.
+     */
+    StudentFields(String fieldName) {
+        this.fieldName = fieldName;
     }
 
+    /**
+     * Get the field name in the database of this field.
+     *
+     * @return Field name in the database.
+     */
     public String getFieldName() {
-        return fieldname;
+        return this.fieldName;
     }
 }
