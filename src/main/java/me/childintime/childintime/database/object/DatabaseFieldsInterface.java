2e7f26dab4549c0fb756f1a58258dec0fa8e2fc1
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-05-31 17:07:40 +0200

    Add DatabaseFieldsInterface Methods

--- src/main/java/me/childintime/childintime/database/object/DatabaseFieldsInterface.java
+++ src/main/java/me/childintime/childintime/database/object/DatabaseFieldsInterface.java
@@ -2,5 +2,16 @@ package me.childintime.childintime.database.object;
 
 public interface DatabaseFieldsInterface {
 
-    String getFieldName();
+    String getDatabaseField();
+
+    /**
+     * Check whether this field is editable by the user.
+     *
+     * @return True if editable, false if not.
+     */
+    boolean isEditable();
+
+    DataType getDataType();
+
+    Class<? extends AbstractDatabaseObject> getReferenceType();
 }
