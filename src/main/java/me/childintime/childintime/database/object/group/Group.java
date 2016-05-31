0e56ac7a084a965c41413117211bada9bc79149a
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-05-31 17:35:47 +0200

    Implement GroupFields enum and Group class

--- src/main/java/me/childintime/childintime/database/object/group/Group.java
+++ src/main/java/me/childintime/childintime/database/object/group/Group.java
@@ -1,5 +1,39 @@
 package me.childintime.childintime.database.object.group;
 
-public class Group {
+import me.childintime.childintime.database.object.AbstractDatabaseObject;
+import me.childintime.childintime.database.object.DatabaseFieldsInterface;
 
+import java.util.List;
+
+public class Group extends AbstractDatabaseObject {
+
+    @Override
+    public boolean hasFields(DatabaseFieldsInterface[] fields) {
+        return false;
+    }
+
+    @Override
+    public boolean hasField(DatabaseFieldsInterface field) {
+        return false;
+    }
+
+    @Override
+    public boolean fetchFields(DatabaseFieldsInterface[] fields) {
+        return false;
+    }
+
+    @Override
+    public boolean fetchField(DatabaseFieldsInterface field) {
+        return false;
+    }
+
+    @Override
+    public List<Object> getFields(DatabaseFieldsInterface[] fields) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Object getField(DatabaseFieldsInterface field) throws Exception {
+        return null;
+    }
 }
--- src/main/java/me/childintime/childintime/database/object/group/GroupFields.java
+++ src/main/java/me/childintime/childintime/database/object/group/GroupFields.java
@@ -1,15 +1,45 @@
 package me.childintime.childintime.database.object.group;
 
-public enum GroupFields {
+import me.childintime.childintime.database.object.AbstractDatabaseObject;
+import me.childintime.childintime.database.object.DataType;
+import me.childintime.childintime.database.object.DatabaseFieldsInterface;
+import me.childintime.childintime.database.object.school.School;
 
-    ID("id"),
-    NAME("name"),
-    SCHOOL_ID("school_id");
+public enum GroupFields implements DatabaseFieldsInterface{
 
-    private String id;
+    ID("id", false, DataType.INTEGER, null),
+    NAME("name", true, DataType.STRING, null),
+    SCHOOL_ID("school_id", false, DataType.REFERENCE, School.class);
 
+    private String databaseField;
+    private boolean editable;
+    private DataType dataType;
+    private Class<? extends AbstractDatabaseObject> referenceType;
 
-    GroupFields(String id) {
-        this.id = id;
+    GroupFields(String databaseField, boolean editable, DataType dataType, Class<? extends AbstractDatabaseObject> referenceType) {
+        this.databaseField = databaseField;
+        this.editable = editable;
+        this.dataType = dataType;
+        this.referenceType = referenceType;
+    }
+
+    @Override
+    public String getDatabaseField() {
+        return databaseField;
+    }
+
+    @Override
+    public DataType getDataType() {
+        return this.dataType;
+    }
+
+    @Override
+    public boolean isEditable() {
+        return this.editable;
+    }
+
+    @Override
+    public Class<? extends AbstractDatabaseObject> getReferenceType() {
+        return this.referenceType;
     }
 }
