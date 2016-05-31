d6776be047b349f6f4b12c9f5915292fe08e1345
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-05-31 17:34:45 +0200

    Implement SchoolFields enum and School class

--- src/main/java/me/childintime/childintime/database/object/school/School.java
+++ src/main/java/me/childintime/childintime/database/object/school/School.java
@@ -1,5 +1,39 @@
 package me.childintime.childintime.database.object.school;
 
-public class School {
+import me.childintime.childintime.database.object.AbstractDatabaseObject;
+import me.childintime.childintime.database.object.DatabaseFieldsInterface;
 
+import java.util.List;
+
+public class School extends AbstractDatabaseObject {
+
+    @Override
+    public boolean hasFields(DatabaseFieldsInterface[] fields) {
+        return false;
+    }
+
+    @Override
+    public boolean hasField(DatabaseFieldsInterface field) {
+        return false;
+    }
+
+    @Override
+    public boolean fetchFields(DatabaseFieldsInterface[] fields) {
+        return false;
+    }
+
+    @Override
+    public boolean fetchField(DatabaseFieldsInterface field) {
+        return false;
+    }
+
+    @Override
+    public List<Object> getFields(DatabaseFieldsInterface[] fields) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Object getField(DatabaseFieldsInterface field) throws Exception {
+        return null;
+    }
 }
