8c34ea6d3ad40bc5119f513da89d6bd42192268c
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-05-31 17:10:48 +0200

    Update TeacherFields class

--- src/main/java/me/childintime/childintime/database/object/teacher/TeacherFields.java
+++ src/main/java/me/childintime/childintime/database/object/teacher/TeacherFields.java
@@ -1,24 +1,47 @@
 package me.childintime.childintime.database.object.teacher;
 
+import me.childintime.childintime.database.object.AbstractDatabaseObject;
+import me.childintime.childintime.database.object.DataType;
 import me.childintime.childintime.database.object.DatabaseFieldsInterface;
+import me.childintime.childintime.database.object.school.School;
 
 public enum TeacherFields implements DatabaseFieldsInterface {
 
-    ID("id"),
-    FIRST_NAME("first_name"),
-    LAST_NAME("last_name"),
-    IS_GYM("is_gym"),
-    SCHOOL_ID("school_id");
+    ID("id", false, DataType.INTEGER, null),
+    FIRST_NAME("first_name", true, DataType.STRING, null),
+    LAST_NAME("last_name", true, DataType.STRING, null),
+    IS_GYM("is_gym", true, DataType.BOOLEAN, null),
+    SCHOOL_ID("school_id", true, DataType.REFERENCE, School.class);
 
-    private String id;
+    private String databaseField;
+    private boolean editable;
+    private DataType dataType;
+    private Class<? extends AbstractDatabaseObject> referenceType;
 
-    TeacherFields(String id) {
-        this.id = id;
+    TeacherFields(String databaseField, boolean editable, DataType dataType, Class<? extends AbstractDatabaseObject> referenceType) {
+        this.databaseField = databaseField;
+        this.editable = editable;
+        this.dataType = dataType;
+        this.referenceType = referenceType;
     }
 
     @Override
-    public String getFieldName() {
-        return null;
+    public String getDatabaseField() {
+        return databaseField;
     }
-    // TODO: implement this
+
+    @Override
+    public DataType getDataType() {
+        return this.dataType;
+    }
+
+    @Override
+    public boolean isEditable() {
+        return this.editable;
+    }
+
+    @Override
+    public Class<? extends AbstractDatabaseObject> getReferenceType() {
+        return this.referenceType;
+    }
 }
