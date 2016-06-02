1a05ed479e21291b20eb0452204b788117ffac58
Tim Visée <timvisee@gmail.com>
2016-06-02 22:45:02 +0200

    Delete unused imports

--- src/main/java/me/childintime/childintime/database/object/measurement/MeasurementManager.java
+++ src/main/java/me/childintime/childintime/database/object/measurement/MeasurementManager.java
@@ -1,6 +1,5 @@
 package me.childintime.childintime.database.object.measurement;
 
-import me.childintime.childintime.database.object.AbstractDatabaseObject;
 import me.childintime.childintime.database.object.AbstractDatabaseObjectManager;
 import me.childintime.childintime.database.object.AbstractDatabaseObjectManifest;
 import me.childintime.childintime.database.object.DatabaseFieldsInterface;
