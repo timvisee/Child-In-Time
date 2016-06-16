8ec959d5ed604ff0c853f222c17ba22cf2e34deb
Tim Visée <timvisee@gmail.com>
2016-06-16 13:31:56 +0200

    Create entity couple reference type name getter

--- src/main/java/me/childintime/childintime/database/entity/AbstractEntityCoupleManifest.java
+++ src/main/java/me/childintime/childintime/database/entity/AbstractEntityCoupleManifest.java
@@ -8,6 +8,42 @@ import java.util.List;
 public abstract class AbstractEntityCoupleManifest extends AbstractEntityManifest {
 
     /**
+     * Get the name of the current entity manager type.
+     *
+     * @param referenceManifest The reference manifest.
+     * @param capital True to make the first character capital.
+     * @param plural True to make the type name a plural.
+     * @param other True to get the name of the other reference.
+     *
+     * @return Entity manager type name.
+     */
+    public String getReferenceTypeName(AbstractEntity referenceManifest, boolean capital, boolean plural, boolean other) {
+        return getReferenceTypeName(referenceManifest, capital, plural, other);
+    }
+
+    /**
+     * Get the name of the current entity manager type.
+     *
+     * @param referenceManifest The reference manifest.
+     * @param capital True to make the first character capital.
+     * @param plural True to make the type name a plural.
+     * @param other True to get the name of the other reference.
+     *
+     * @return Entity manager type name.
+     */
+    public String getReferenceTypeName(AbstractEntityManifest referenceManifest, boolean capital, boolean plural, boolean other) {
+        // Get the manifest to use
+        AbstractEntityManifest manifest = referenceManifest;
+
+        // Get the other manifest
+        if(other)
+            manifest = getOtherManifest(manifest);
+
+        // Get the reference type name
+        return (capital ? "C" : "c") + "oupled " + manifest.getTypeName(false, plural);
+    }
+
+    /**
      * Get the manifest of the field that isn't of the same entity type as the given.
      * If you have the entity couple fields A and B with a reference to their entity types, and A is given,
      * the manifest of B is returned. The same goes the other way around, if B is given, A is returned.
